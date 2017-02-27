baa1baeb4526b8c48e16b1ab4f055c7802cc4980
沈怡安 <delight1001003@gmail.com>
2017-02-27 16:25:45 +0800

    TextFileSample001 done

--- TextFileSample001/Form1.cs
+++ TextFileSample001/Form1.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace TextFileSample001
 {
     public partial class Form1 : Form
     {
+        private string fileName;
         public Form1()
         {
             InitializeComponent();
+            fileName = "test.txt";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear(); //按"讀取"鍵後,把textBox的內容清除
+
+            if(File.Exists(fileName))
+            {
+                textBox1.Text = File.ReadAllText(fileName);
+            }
+            else
+            {
+                MessageBox.Show("檔案不存在");
+            }
+            
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(fileName, textBox1.Text);
+            MessageBox.Show("存檔完成");
         }
     }
 }
